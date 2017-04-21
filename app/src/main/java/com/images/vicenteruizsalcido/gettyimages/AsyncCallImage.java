f79f59d444d6c0e2d8434416eb4425bf27cae3f4
DIR\vicente.ruiz.salcido <raul.x.ruiz.-nd@disney.com>
2017-04-21 01:15:29 -0400

    Clean service call

--- app/src/main/java/com/images/vicenteruizsalcido/gettyimages/AsyncCallImage.java
+++ app/src/main/java/com/images/vicenteruizsalcido/gettyimages/AsyncCallImage.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import com.google.gson.Gson;
 import com.images.vicenteruizsalcido.gettyimages.response.PayLoad;
 
+import java.io.IOException;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -15,6 +17,11 @@ import okhttp3.Response;
  */
 
 public class AsyncCallImage extends AsyncTask<String, Void, Void> {
+
+    private static final String GETTY_KEY = "n5ttb3egk6592kv4wde4yhtn";
+    private static final String GETTY_URL = "https://api.gettyimages.com/v3/search/images/creative?phrase=";
+    private static final String API_KEY = "Api-Key";
+
     private MainFragment.FragmentCallback fragmentCallback;
     private PayLoad payLoad;
 
@@ -23,20 +30,17 @@ public class AsyncCallImage extends AsyncTask<String, Void, Void> {
     }
     @Override
     public Void doInBackground(String... params) {
-        String search = params[0];
-        String key = "n5ttb3egk6592kv4wde4yhtn";
-        String Url = "https://api.gettyimages.com/v3/search/images/creative?phrase=" + search;
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url(Url)
-                .addHeader("Api-Key", key)
+                .url(GETTY_URL + params[0])
+                .addHeader(API_KEY, GETTY_KEY)
                 .build();
         try {
             Response response = client.newCall(request).execute();
             String s = response.body().string();
             Gson gson = new Gson();
             payLoad = gson.fromJson(s, PayLoad.class);
-        } catch (Exception e) {
+        } catch (IOException e) {
             Log.i("", e.getMessage());
         }
         return null;
@@ -61,5 +65,4 @@ public class AsyncCallImage extends AsyncTask<String, Void, Void> {
     public PayLoad getPayLoad() {
         return payLoad;
     }
-
 }
